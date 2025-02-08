24aa13f32788e955b70e414eedda279611f63fe8
michaelajaluvkova <135652256+michaelajaluvkova@users.noreply.github.com>
2025-02-08 15:27:47 +0100

    Add files via upload

---
+++ playground.py
@@ -0,0 +1,12 @@
+from RevenueMaker import RevenueMakerClass
+import pandas as pd
+
+
+revenue = RevenueMakerClass(include_support_cost=True,
+                            filter_by_year=False,
+                            switch_product_group=False,
+                            column1='',
+                            column2='')
+
+revenue.create_presentation()
+revenue.one_big_graph()
